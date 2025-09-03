3450fe76e9b211f09e8dabdd8c84db190dc3cbd5
ninewiki9 <ninewiki9@gmail.com>
2025-09-03 16:17:13 +0900

    Delete 2.vpc.tf

--- 2.vpc.tf
+++
@@ -1,8 +0,0 @@
-resource "aws_vpc" "ninewiki-test-vpc" {
-  cidr_block           = "10.10.0.0/16"
-  enable_dns_support   = true           # VPC 내에서 DNS 쿼리를 처리할지 여부를 결정
-  enable_dns_hostnames = true       # VPC 내에서 DNS 호스트 이름을 활성화할지 여부를 결정
-  tags = {
-    "Name" = "ninewiki-test-vpc"
-  }
-}
